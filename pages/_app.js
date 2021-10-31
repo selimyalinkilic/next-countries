d54d03b75d75d7110f20525d9eaf060731d87318
selimyalinkilic <yalinkilicselim@gmail.com>
2021-10-31 16:47:40 +0300

    Added color mode

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,9 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import theme from "../styles/theme";
 
 const App = ({ Component, pageProps }) => {
   return (
-    <ChakraProvider>
+    <ChakraProvider resetCSS theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   );
