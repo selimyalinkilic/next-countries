fa60ba54de9f661672499fd045490adb1f611789
selimyalinkilic <yalinkilicselim@gmail.com>
2021-11-15 01:22:55 +0300

    Added color mode and updated by color mode

--- components/header.js
+++ components/header.js
@@ -1,11 +1,11 @@
-import { Heading } from "@chakra-ui/react";
-import { MoonIcon } from "@chakra-ui/icons";
-import { Box, Flex } from "@chakra-ui/layout";
-import { IconButton } from "@chakra-ui/react";
-import { useColorMode } from "@chakra-ui/color-mode";
+import { Heading } from '@chakra-ui/react'
+import { MoonIcon, SunIcon } from '@chakra-ui/icons'
+import { Flex } from '@chakra-ui/layout'
+import { IconButton } from '@chakra-ui/react'
+import { useColorMode } from '@chakra-ui/color-mode'
 
 const Header = () => {
-  const { toggleColorMode } = useColorMode();
+  const { colorMode, toggleColorMode } = useColorMode()
   return (
     <Flex
       height={[12, 16, 24]}
@@ -14,17 +14,26 @@ const Header = () => {
       alignItems="center"
       px={[4, 8, 16]}
     >
-      <Heading as="h1" fontSize={["xl", "2xl", "3xl", "4xl"]} color="gray.500">
-        {" "}
-        &#123; Next Countries &#125;{" "}
+      <Heading
+        as="h1"
+        fontSize={['xl', '2xl', '3xl', '4xl']}
+        color={colorMode === 'dark' ? 'gray.100' : 'gray.500'}
+      >
+        &#123; Next Countries &#125;
       </Heading>
       <IconButton
+        bg="transparent"
+        color={colorMode === 'dark' ? 'gray.100' : 'gray.500'}
+        _hover={{
+          backgroundColor: colorMode === 'dark' ? 'whiteAlpha.100' : 'gray.100',
+          color: colorMode === 'dark' ? 'gray.300' : 'gray.500'
+        }}
         aria-label="Change Color Mode"
-        icon={<MoonIcon />}
+        icon={colorMode === 'dark' ? <SunIcon /> : <MoonIcon />}
         onClick={toggleColorMode}
       />
     </Flex>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
