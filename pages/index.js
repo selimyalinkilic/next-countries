7defd1409367433a834b78074e0ffeef84d3b7a9
selimyalinkilic <yalinkilicselim@gmail.com>
2021-11-16 01:54:08 +0300

    Search bar and search function added

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,72 @@
-import { Container, Grid } from '@chakra-ui/layout'
+import { Container, Flex, Grid } from '@chakra-ui/layout'
+import { Spinner } from '@chakra-ui/spinner'
+import axios from 'axios'
+import { useCallback, useEffect, useState } from 'react'
 import Card from '../components/card'
 import Layout from '../components/layout'
+import SearchBar from '../components/searchBar'
 import Country from '../model/country'
 
 const Home = ({ countries }) => {
+  const [search, setSearch] = useState('')
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(false)
+  const handleSearchChange = (val) => {
+    if (val.length >= 3) setSearch(val)
+    else {
+      setSearch('')
+      setData(countries)
+    }
+  }
+
+  const searchByName = async (val) => {
+    return await axios
+      .get(`${process.env.NEXT_PUBLIC_API_URL}/name/${val}`)
+      .then((res) => {
+        setLoading(true)
+        setTimeout(() => {
+          setLoading(false)
+          setData(res.data)
+        }, 1000)
+      })
+  }
+
+  useEffect(() => {
+    if (search) searchByName(search)
+    else setData(countries)
+  }, [search])
+
   return (
     <Layout>
       <Container maxW="container.xl">
-        <Grid templateColumns="repeat(4, 1fr)" gap={12} mt={10}>
-          {countries?.map((item, index) => (
-            <Card
-              key={index}
-              flag={item.flags.png}
-              name={item.name}
-              population={item.population}
-              region={item.region}
-              capital={item.capital}
-            />
-          ))}
-        </Grid>
+        <Flex mt={10}>
+          <SearchBar handleSearchChange={handleSearchChange} />
+        </Flex>
+        {loading ? (
+          <Spinner color="red.500" />
+        ) : (
+          <Grid templateColumns="repeat(4, 1fr)" gap={12} mt={10}>
+            {data?.map((item, index) => (
+              <Card
+                key={index}
+                flag={item.flags.png}
+                name={item.name}
+                population={item.population}
+                region={item.region}
+                capital={item.capital}
+              />
+            ))}
+          </Grid>
+        )}
       </Container>
     </Layout>
   )
 }
 
 export const getStaticProps = async () => {
-  const data = Country.all()
-  const response = await Promise.all([data])
+  const all = Country.all()
+
+  const response = await Promise.all([all])
 
   return {
     props: {
