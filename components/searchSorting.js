a41eca1fbb10c8b4ef09cd9cf6e0894e99b3d4a7
selimyalinkilic <yalinkilicselim@gmail.com>
2021-11-18 01:34:26 +0300

    Country sorting added

---
+++ components/searchSorting.js
@@ -0,0 +1,26 @@
+import { Select } from '@chakra-ui/select'
+
+const regions = [
+  { id: 1, name: 'Africa' },
+  { id: 2, name: 'Americas' },
+  { id: 3, name: 'Asia' },
+  { id: 4, name: 'Europe' },
+  { id: 5, name: 'Oceania' }
+]
+
+const SearchSorting = ({ handleFilterChange }) => {
+  const handleChange = (e) => {
+    handleFilterChange(e.target.value)
+  }
+  return (
+    <Select placeholder="Filter by Region" onChange={handleChange}>
+      {regions.map((item) => (
+        <option key={item.id} value={item.name.toLowerCase()}>
+          {item.name}
+        </option>
+      ))}
+    </Select>
+  )
+}
+
+export default SearchSorting
