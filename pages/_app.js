8d962559c911db086a441c9d5dd950e5786e84df
selimyalinkilic <yalinkilicselim@gmail.com>
2021-11-18 01:35:56 +0300

    Meta tag added for responsive

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
-import { ChakraProvider } from "@chakra-ui/react";
-import theme from "../styles/theme";
+import { ChakraProvider } from '@chakra-ui/react'
+import theme from '../styles/theme'
+import Head from 'next/head'
 
 const App = ({ Component, pageProps }) => {
   return (
     <ChakraProvider resetCSS theme={theme}>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <Component {...pageProps} />
     </ChakraProvider>
-  );
-};
-export default App;
+  )
+}
+export default App
