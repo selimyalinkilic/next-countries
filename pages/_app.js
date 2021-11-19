fb3236d3c51ffbf8815f0fc6056f9657e83f8aa9
selimyalinkilic <yalinkilicselim@gmail.com>
2021-11-19 23:23:17 +0300

    Added new page progressbar loader

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import theme from '../styles/theme'
 import Head from 'next/head'
+import NextNProgress from 'nextjs-progressbar'
 
 const App = ({ Component, pageProps }) => {
   return (
@@ -8,6 +9,13 @@ const App = ({ Component, pageProps }) => {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
+      <NextNProgress
+        color="#29D"
+        startPosition={0.1}
+        stopDelayMs={200}
+        height={2}
+        showOnShallow={true}
+      />
       <Component {...pageProps} />
     </ChakraProvider>
   )
